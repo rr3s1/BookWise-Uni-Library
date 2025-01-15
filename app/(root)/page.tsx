44181d323c8df71574a76bb8e3c6dea3729155e7
aessary <raressilviulazar@gmail.com>
2025-01-15 09:51:44 +0000

    Home Data - BookOverview

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,18 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { Button } from "@/components/ui/button";
+import { sampleBooks } from "@/constants";
 
 
 const Home = () => 
   (
     <>
-    <BookOverview />
-    <BookList />
+    <BookOverview {...sampleBooks[0]}/>
+    <BookList
+    title = "Latest Books"
+    books={sampleBooks}
+    containerClassBane="mt-22"
+    />
     </>
   );
 
