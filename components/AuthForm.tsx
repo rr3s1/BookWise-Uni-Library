882afe98ee4e80209e1fcc4196a19401a9b065d5
aessary <raressilviulazar@gmail.com>
2025-04-19 11:17:55 +0100

    Dynamic Auth with Zod Validation

---
+++ components/AuthForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthForm = ({type, schema, defaultValues, onSubmit}: Props) => {
+  return (
+    <div>
+      AuthForm -- {type}
+    </div>
+  )
+}
+
+export default AuthForm
