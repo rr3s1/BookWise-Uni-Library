f4e732f055d2a6f11570dc3710335ec8ab2e41a3
aessary <raressilviulazar@gmail.com>
2025-04-21 07:58:05 +0100

    DB connect & Data Retrieve

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,16 @@ import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { Button } from "@/components/ui/button";
 import { sampleBooks } from "@/constants";
+import {db} from "@/database/drizzle";
+import {users} from "@/database/schema";
 
 
-const Home = () => 
-  (
+const Home = async () => {
+  const result = await db.select().from(users);
+  console.log(JSON.stringify(result, null, 2));
+
+
+  return (
     <>
     <BookOverview {...sampleBooks[0]}/>
     <BookList
@@ -14,7 +20,7 @@ const Home = () =>
     containerClassBane="mt-22"
     />
     </>
-  );
+  );}
 
 
 export default Home;
